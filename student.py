5d496ab6ddc2403853cf5227665664be3fcde561
Louka Babic <louka@babicconsulting.com>
2019-11-13 15:01:33 -0500

    changing motor power to stop spin

--- student.py
+++ student.py
@@ -17,7 +17,7 @@ class Piggy(PiggyParent):
         MAGIC NUMBERS <-- where we hard-code our settings
         '''
         self.LEFT_DEFAULT = 80
-        self.RIGHT_DEFAULT = 80
+        self.RIGHT_DEFAULT = 77
         self.SAFE_DIST = 250
         self.MIDPOINT = 1500  # what servo command (1000-2000) is straight forward for your bot?
         self.load_defaults()
