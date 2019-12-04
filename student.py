ba4189b943248005b40f14225bdd4c2a6a3cb8bd
Louka Babic <louka@babicconsulting.com>
2019-12-04 11:02:01 -0500

    adding underscore to get_heading

--- student.py
+++ student.py
@@ -60,7 +60,7 @@ class Piggy(PiggyParent):
         startheading = self.get_heading()
 
         while True:
-            if abs(startheading - self.getheading()) > 5:
+            if abs(startheading - self.get_heading()) > 5:
                 self.turn_to_deg(startheading)
 
     def waggle(self):
